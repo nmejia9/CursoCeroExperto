7c0687351618de95688f36ca4d05d18059dd95e4
nmejia9 <norbey-mejia@hotmail.com>
2023-08-11 16:17:00 -0500

    leccion 1 - variables

---
+++ 00_hello.py
@@ -0,0 +1,16 @@
+# Esto es un Comentario 
+
+# Hola Mundo en Python
+print("Hola Mundo")
+print('Hola Mundo 2')
+
+""" 
+Comentario 
+en varias 
+lineas  
+"""
+#Consultar el tipo de dato 
+print(type("Soy un Dato str")) #Tipo  'str'
+print(type(5)) #Tipo  'int'
+print(type(5.5)) #Tipo  'float'
+print(type(True)) #Tipo  'Blooleano'
